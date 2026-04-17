ffb8fe72e3d5684dff794dab56f7dc3d59bd7883
ujjwalkirti <kirti.chargers@gmail.com>
2026-04-17 21:26:41 +0530

    fix: remove NativeWind from metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,6 @@
 // metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = config;
